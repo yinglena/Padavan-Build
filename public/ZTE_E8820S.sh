6159f4ed718319bcc36a62a1774c8feb28062735
yinglena <81199302+yinglena@users.noreply.github.com>
2021-08-09 12:01:35 +0800

    Update ZTE_E8820S.sh

--- public/ZTE_E8820S.sh
+++ public/ZTE_E8820S.sh
@@ -1,6 +1,6 @@
 lan_ip="192.168.2"                          # LAN 地址 别写后面的 .1
 default_path="./user/shared"                # 默认配置路径
-config="./configs/templates/PSG1218.config" # 默认配置文件
+config="./configs/templates/ZTE_E8820S.config" # 默认配置文件
 
 echo "修改 LAN IP 地址"
 sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
