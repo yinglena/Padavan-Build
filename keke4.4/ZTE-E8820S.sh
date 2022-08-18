d7a96f46d2147c84d2efb9d307ba30327dce4bc7
yinglena <81199302+yinglena@users.noreply.github.com>
2022-08-18 16:25:10 +0800

    Create ZTE-E8820S.sh

---
+++ keke4.4/ZTE-E8820S.sh
@@ -0,0 +1,13 @@
+lan_ip="192.168.2"                          # LAN 地址 别写后面的 .1
+default_path="./user/shared"                # 默认配置路径
+config="./configs/templates/ZTE-E8820S.config" # 默认配置文件
+
+echo "修改 LAN IP 地址"
+sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
+
+
+# echo "打开2G无线桥接"
+ sed -i 's/{ "rt_mode_x", "0" }/{ "rt_mode_x", "4" }/g' $default_path/defaults.c
+
+# echo "打开5G无线桥接"
+ sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
